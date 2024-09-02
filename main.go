1142a2e2e660cdbb63298064b1a27c5b5e13a9e9
Dusong <dusong700@gmail.com>
2024-09-02 16:02:29 +0800

    comment

--- main.go
+++ main.go
@@ -29,10 +29,8 @@ func init() {
 func main() {
 	router := gin.Default()
 
-	// 提供静态文件服务
 	router.Static("/front", "./front")
 
-	// 提供首页访问
 	router.GET("/", func(c *gin.Context) {
 		c.File("./front/index.html")
 	})
@@ -54,7 +52,7 @@ func main() {
 	}
 
 	user.POST("/refresh-token", controller.RefreshJWT)
-	// 显示主页
+
 	router.GET("/home", func(c *gin.Context) {
 		c.String(http.StatusOK, "欢迎来到投标专家评审系统的主页!")
 	})
@@ -63,7 +61,6 @@ func main() {
 	protected.Use(controller.JWTMiddleware())
 	{
 		protected.GET("/verify", controller.VerifyHandler)
-		// 其他需要验证的路由
 	}
-	router.Run(":8080") // 在8080端口启动服务
+	router.Run(":8080")
 }
